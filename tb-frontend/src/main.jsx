2589d32eef4e4833663265c2d3d5a9d2466e68bb
Thato Mongwe <thatomongwe47@gmail.com>
2024-12-29 21:48:38 +0200

    Update main.jsx

--- tb-frontend/src/main.jsx
+++ tb-frontend/src/main.jsx
@@ -1,10 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import "./index.css"; // Include Tailwind or custom CSS
-import "../tailwind.config.js";
-import "../vite.config.js";
-import "../index.html";
+import './styles/global.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
