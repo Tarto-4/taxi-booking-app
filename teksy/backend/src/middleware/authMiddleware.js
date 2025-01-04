2e99fff84a2180ac8ae93afa98d0b728c3918aa8
Thato Mongwe <thatomongwe47@gmail.com>
2025-01-04 02:02:17 +0000

    rename and update

---
+++ teksy/backend/src/middleware/authMiddleware.js
@@ -0,0 +1,8 @@
+exports.authMiddleware = (req, res, next) => {
+  const token = req.headers.authorization?.split(' ')[1];
+  if (!token || !token.startsWith('mockToken')) {
+      return res.status(403).json({ message: 'Unauthorized' });
+  }
+  req.user = { id: parseInt(token.split('-')[1], 10) };
+  next();
+};
