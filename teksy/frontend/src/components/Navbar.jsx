785cd08ba7bee15cd4179e5560b1594bb0c3ac64
Thato Mongwe <thatomongwe47@gmail.com>
2025-01-04 20:56:44 +0200

    Update Navbar.jsx

--- teksy/frontend/src/components/Navbar.jsx
+++ teksy/frontend/src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const Navbar = () => {
   return (
     <div className="navbar">
-      <h1>Taxi Booking App</h1>
+      <h1>TaxiGo</h1>
       <div>
         <Link to="/">Home</Link>
         <Link to="/login">Login</Link>
