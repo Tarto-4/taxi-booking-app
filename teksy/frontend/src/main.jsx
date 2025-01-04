4feb215907229565a7990e4ac2c4e5654b6eb4b6
Thato Mongwe <thatomongwe47@gmail.com>
2025-01-04 15:10:16 +0200

    Update main.jsx

--- teksy/frontend/src/main.jsx
+++ teksy/frontend/src/main.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import App from './App';
 import { AuthProvider } from './hooks/useAuth';
+import './styles/styles.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.render(
   <React.StrictMode>
     <AuthProvider>
       <App />
     </AuthProvider>
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
