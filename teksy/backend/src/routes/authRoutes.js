8c2aebc2f2c92769d0565ec22aa304d78a3a3d23
Thato Mongwe <thatomongwe47@gmail.com>
2025-01-06 19:48:33 +0200

    Defines routes for authentication

--- teksy/backend/src/routes/authRoutes.js
+++ teksy/backend/src/routes/authRoutes.js
@@ -1,8 +1,9 @@
-const express = require('express');
-const { registerUser, loginUser } = require('../controllers/authController'); // Correct import names
+const express = require("express");
+const { registerUser, loginUser } = require("../controllers/authController");
+
 const router = express.Router();
 
-router.post('/register', registerUser); // Match the exported function name
-router.post('/login', loginUser);       // Match the exported function name
+router.post("/register", registerUser);
+router.post("/login", loginUser);
 
 module.exports = router;
