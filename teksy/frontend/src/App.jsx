d0d582f56993236b44031e8ee7936ec8cb929b9d
Thato Mongwe <thatomongwe47@gmail.com>
2025-01-07 09:51:30 +0200

    Update App.jsx

--- teksy/frontend/src/App.jsx
+++ teksy/frontend/src/App.jsx
@@ -4,6 +4,7 @@ import Header from './components/layout/Header';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import Dashboard from './pages/Dashboard';
+import Home from './pages/Home';
 
 function App() {
     return (
@@ -13,6 +14,7 @@ function App() {
                 <Route path="/login" element={<Login />} />
                 <Route path="/register" element={<Register />} />
                 <Route path="/dashboard" element={<Dashboard />} />
+                <Route path="/home" element={<Home />} />
             </Routes>
         </Router>
     );
