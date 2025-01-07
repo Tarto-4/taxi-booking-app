2209fab26e924fd5c1483327c2327f128ba3539a
Thato Mongwe <thatomongwe47@gmail.com>
2025-01-07 10:34:14 +0200

    Update Header.jsx

--- teksy/frontend/src/components/layout/Header.jsx
+++ teksy/frontend/src/components/layout/Header.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import SignOutButton from './SignOutButton';
+import logo from './logo.png.webp';
 
 const Header = () => (
     <header className="header">
         <div class="logo-container">
-    <img src="path/to/logo.png" alt="Logo" class="logo" />
+    <img src="/logo.png" alt="Logo" class="logo" />
 </div>
         <h1 className="title">Taxi Booking App</h1>
         <nav>
