6259130fc7c86d4b70b07ff004992d4871caff18
Thato Mongwe <thatomongwe47@gmail.com>
2025-01-07 10:35:23 +0200

    Update Header.jsx

--- teksy/frontend/src/components/layout/Header.jsx
+++ teksy/frontend/src/components/layout/Header.jsx
@@ -6,7 +6,7 @@ import logo from './logo.png.webp';
 const Header = () => (
     <header className="header">
         <div class="logo-container">
-    <img src="/logo.png" alt="Logo" class="logo" />
+    <img src="/images/logo.png" alt="Logo" class="logo" />
 </div>
         <h1 className="title">Taxi Booking App</h1>
         <nav>
