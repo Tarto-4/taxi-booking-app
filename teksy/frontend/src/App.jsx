16c4fed72efa5b30d730e3b3aee9a35051f62261
Thato Mongwe <thatomongwe47@gmail.com>
2025-01-07 12:54:24 +0200

    Add files via upload

--- teksy/frontend/src/App.jsx
+++ teksy/frontend/src/App.jsx
@@ -1,21 +1,32 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import Login from './pages/Login';
-import Register from './pages/Register';
+import Register from './pages/Register'; // Import Register component
 import Dashboard from './pages/Dashboard';
-import "./styles/styles.css";
+import ProtectedRoute from './components/ProtectedRoute';
+import './styles/styles.css';
 
-function App() {
+const App = () => {
   return (
     <Router>
+      <Navbar />
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/register" element={<Register />} /> {/* Add Register route */}
+        <Route
+          path="/dashboard"
+          element={
+            <ProtectedRoute>
+              <Dashboard />
+            </ProtectedRoute>
+          }
+        />
       </Routes>
     </Router>
   );
-}
+};
 
 export default App;
