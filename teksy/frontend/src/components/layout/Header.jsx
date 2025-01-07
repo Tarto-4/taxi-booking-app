59bd367b0de7c15914b8874e04aa98776e61a23d
Thato Mongwe <thatomongwe47@gmail.com>
2025-01-07 12:00:11 +0200

    Update Header.jsx

--- teksy/frontend/src/components/layout/Header.jsx
+++ teksy/frontend/src/components/layout/Header.jsx
@@ -10,6 +10,7 @@ const Header = () => (
 </div>
         <h1 className="title">Taxi Booking App</h1>
         <nav>
+            <Link to="/home"</Link>
             <Link to="/login">Login</Link>
             <Link to="/register">Register</Link>
             <Link to="/dashboard">Dashboard</Link>
