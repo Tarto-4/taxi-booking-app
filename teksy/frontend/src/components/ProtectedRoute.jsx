7c69cfcffae87e2b2a56d5986d2071fe32570172
Thato Mongwe <thatomongwe47@gmail.com>
2025-01-07 21:05:59 +0200

    Update ProtectedRoute.jsx

--- teksy/frontend/src/components/ProtectedRoute.jsx
+++ teksy/frontend/src/components/ProtectedRoute.jsx
@@ -1,9 +1,10 @@
-import React from 'react';
-import { Navigate } from 'react-router-dom';
+import React from "react";
+import { Navigate } from "react-router-dom";
 
 const ProtectedRoute = ({ children }) => {
-  const token = localStorage.getItem('token');
-  return token ? children : <Navigate to="/" />;
+  const isAuthenticated = localStorage.getItem("token");
+
+  return isAuthenticated ? children : <Navigate to="/login" />;
 };
 
 export default ProtectedRoute;
