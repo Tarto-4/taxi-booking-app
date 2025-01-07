000ccbd34838debca3392efdfb40fdad18fb9ef2
Thato Mongwe <thatomongwe47@gmail.com>
2025-01-07 12:58:09 +0200

    Add files via upload

--- teksy/frontend/src/components/layout/Header.jsx
+++ teksy/frontend/src/components/layout/Header.jsx
@@ -1,16 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import SignOutButton from './SignOutButton';
-import logo from './logo.png.';
+import logo from '../public/logo.png'; // Assuming logo is stored in public/logo.png
 
 const Header = () => (
     <header className="header">
-        <div class="logo-container">
-    <img src={logo} alt="Logo" className="logo" />
-</div>
+        <img src={logo} alt="Taxi Booking Logo" className="logo" />
         <h1 className="title">Taxi Booking App</h1>
         <nav>
-            <Link to="/home"</Link>
             <Link to="/login">Login</Link>
             <Link to="/register">Register</Link>
             <Link to="/dashboard">Dashboard</Link>
