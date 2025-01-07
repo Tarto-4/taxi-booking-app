b806c572d3453e39f9300d6d977cf4e41eba9b09
Thato Mongwe <thatomongwe47@gmail.com>
2025-01-07 11:57:49 +0200

    Update App.jsx

--- teksy/frontend/src/App.jsx
+++ teksy/frontend/src/App.jsx
@@ -9,7 +9,7 @@ import "./styles/styles.css";
 const App = () => (
   <Router>
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="home" element={<Home />} />
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
       <Route path="/dashboard" element={<Dashboard />} />
